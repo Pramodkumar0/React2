98b33c1d5caa7e4dafc7512d17e703d5ed7464c9
pramodkumar0 <pramodpusala@gmail.com>
2022-10-31 11:48:36 +0530

    ass2

---
+++ src/Components/Form.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+const Form = ({name,setName,dept,setDept,rating,setRating,users,setusers,}) => {
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleDeptChange = (e) => {
+    setDept(e.target.value);
+  };
+
+  const handleRatingChange = (e) => {
+    setRating(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setusers([...users, { name: name, dept: dept, rating: rating }]);
+    console.log(users);
+  };
+
+  return (
+    <>
+        <h1> EMPLOYEE FEEDACK FORM </h1>
+
+      <form onSubmit={handleSubmit} className="formdata">
+
+        <label>Name:<input onChange={handleNameChange}value={name}className="textbox"   type="text"name="name"/></label>      
+
+        <label>Department:<input onChange={handleDeptChange}value={dept} className="textbox"
+            type="text" name="dept" required/></label>     
+
+        <label>Rating :<input onChange={handleRatingChange}value={rating}className="textbox"type="text"name="rating" required/></label>       
+        
+        <br/>
+        <br/> 
+        <input className="btn" type="submit" value="Submit"/>
+        
+      </form>
+    </>
+  );
+};
+
+export default Form;
